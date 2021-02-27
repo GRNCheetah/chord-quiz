dfdee70a90f15263f30c80c13d6baada6b75e440
Gavin <grncheetah10@gmail.com>
2021-02-26 18:01:38 -0600

    Can quiz, play noise, repeat it, and reveal the answer. Seems to function good

--- func.js
+++ func.js
@@ -5,7 +5,7 @@ ANSWER = document.getElementById('answer');
 function quiz() {
 
     // Clear answer
-    
+    document.getElementById('answer').innerHTML = "";
 
     // Gen list of checked boxes
     var checkboxes = document.getElementsByName('chord');
@@ -13,7 +13,6 @@ function quiz() {
     for (var checkbox of checkboxes) {
         if (checkbox.checked) {
             to_choose_from.push(checkbox);
-            console.log("Found:");
         }
     }
 
@@ -27,18 +26,37 @@ function quiz() {
     const ind = Math.floor(Math.random() * to_choose_from.length);
     curr_sound = to_choose_from[ind];
 
+    // Little bit of user feedback
+    document.getElementById('answer').innerHTML = "*********";
+
+    // Play sound
     var audio = new Audio('Chords/'+to_choose_from[ind].value);
     audio.play();
-
 }
 
 function repeat() {
+    // Only play if there is a sound selected
     if (curr_sound) {
         var audio = new Audio('Chords/' + curr_sound.value);
         audio.play();
+    } else {
+        alert("No sound selected");
+    }
+}
+function clear_checks() {
+    // NOT USED
+    // Clear the checkboxes and reset everything
+    console.log("ldl");
+    document.getElementById('answer').innerHTML = "*********";
+    curr_sound = "";
+    var checkboxes = document.getElementsByName('chord');
+    for (var checkbox of checkboxes) {
+        checkbox.check = false;
     }
 }
 
 function reveal() {
-    console.log(curr_sound);
+    if (curr_sound) {
+        document.getElementById('answer').innerHTML = curr_sound.id;
+    }
 }
